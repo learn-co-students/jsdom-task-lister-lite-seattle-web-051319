e645164289c72a37179dd9e9224f0174c49875a1
Noah Fairbairn <noahfairbairn@gmail.com>
2019-06-25 11:00:20 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+    function addEventToList() {
+        const form = document.getElementById("create-task-form");
+        const input = document.getElementById("new-task-description");
+        const list = document.getElementById("list")
+        
+
+        form.addEventListener("submit", (ev) => {
+          
+          ev.preventDefault()
+        
+          let li = document.createElement("li")
+
+          li.textContent = input.value
+          // debugger;
+          list.appendChild(li)
+
+          // add delete button for list things
+
+          let deleteButton = document.createElement("span")
+
+          deleteButton.textContent = "[x]"
+
+          li.appendChild(deleteButton)
+
+          deleteButton.addEventListener("click", () => {
+            li.remove()
+          })
+
+        })
+    }
+    addEventToList()
 });
