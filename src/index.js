7de6a50a5a5b622e8c7eb2fca8fa8eb415cbcb93
fut0k0 <cccabell@icloud.com>
2019-06-27 11:04:49 -0700

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  manageToDo();
+  
 });
+
+
+function manageToDo() {	
+	const taskDescrip = document.getElementById("new-task-description");
+	const taskForm = document.getElementById("create-task-form");
+	const taskList = document.getElementById("tasks")
+			
+	taskForm.addEventListener("submit", function(event) {
+		event.preventDefault();
+		
+		let task = document.createElement("li");
+		task.textContent = taskDescrip.value;
+		taskList.appendChild(task);
+		
+		let deleteButton = document.createElement("button");
+		deleteButton.textContent = "X";
+		task.appendChild(deleteButton);
+		
+		deleteButton.addEventListener("click", function() {
+			deleteButton.parentElement.remove();
+		});
+							
+		taskForm.reset();
+	});
+}
