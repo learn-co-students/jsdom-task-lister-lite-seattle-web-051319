f19529f4b3292f1896c4ee60b3bfb6d81cce66e0
ndnguyen07 <github email address>
2019-07-01 15:06:12 -0700

    first try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  main();
 });
+
+function main(){
+  addtask();
+}
+
+function addtask(){
+  const form = getElementById('create-task-form')
+  const input = getElementById('new-task-description')
+
+  form addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let li = document.createElement('li')
+    let button = document.createElement('button')
+    li.textContent = text
+  })
+}
